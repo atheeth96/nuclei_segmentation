ad6357269a8e468780be5df96b5b7a37602d68f0
atheeth96 <atheeth14@gmail.com>
2020-04-15 13:47:18 +0900

    Adding python scripts

--- Utils/Prediction.py
+++ Utils/Prediction.py
@@ -25,7 +25,92 @@ import cv2
 
 
 
+def whole_img_pred2(IMAGE_PATH,img_name,model,print_prompt=False):
+    
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model=model.to(device)
+    model.eval()
+    
+        
+    step=512
+    avg_dice=0
+  
 
+
+    input_image=imread(os.path.join(IMAGE_PATH+'/ROI',img_name)).astype(np.float32)
+#     boundary_mask=imread(os.path.join(IMAGE_PATH+'/bound_mask',img_name))
+#     nuclei_mask=imread(os.path.join(IMAGE_PATH+'/nuc_mask',img_name))
+
+#     y_true=np.stack((nuclei_mask,boundary_mask),axis=0)
+#     y_true=np.expand_dims(y_true,axis=0)
+
+    r,c=input_image.shape#4663,3881
+
+    new_r_count=(math.ceil((r-512)/512)+1)#5
+    new_c_count=(math.ceil((c-512)/512)+1)#5
+
+
+    pad_r1=((new_r_count-1)*512-r+512)//2 #200
+    pad_r2=((new_r_count-1)*512-r+512)-pad_r1 #200
+    pad_c1=((new_c_count-1)*512-c+512)//2 #0
+    pad_c2=((new_c_count-1)*512-c+512)-pad_c1#0
+
+    image_padded=np.pad(input_image, [(pad_r1,pad_r2),(pad_c1,pad_c2)], 'constant', constant_values=0)
+
+
+    window_shape=(512,512)
+
+    img_patches=skimage.util.view_as_windows(image_padded, window_shape, step=step)
+    img_patches=img_patches.reshape((-1,512,512))
+    img_patches=img_patches.transpose((0,2,1))
+    img_patches=np.expand_dims(img_patches,axis=1)
+
+    max_patch_level=np.amax(img_patches,axis=(1,2,3)).reshape(-1)
+    for i in range(img_patches.shape[0]):
+        img_patches[i]=img_patches[i]/max_patch_level[i]
+
+
+    bound_temp=[]
+    nuclei_temp=[]
+
+    for i in range(new_r_count):
+
+        temp_img_patches=torch.from_numpy(img_patches[i*new_r_count:(i+1)*new_r_count]).type(torch.FloatTensor).to(device)
+        pred=torch.sigmoid(model(temp_img_patches))
+        del temp_img_patches
+
+        nuclei,bound=torch.chunk(pred,2,dim=1)
+        del pred
+        nuclei,bound=nuclei.detach().cpu().numpy(),bound.detach().cpu().numpy()
+
+        nuclei=np.squeeze(nuclei,axis=1).transpose((0,2,1))
+        nuclei=np.concatenate(nuclei,axis=1)
+        nuclei_temp.append(nuclei)
+
+        bound=np.squeeze(bound,axis=1).transpose((0,2,1))
+        bound=np.concatenate(bound,axis=1)
+        bound_temp.append(bound)
+
+    nuclei_temp=np.array(nuclei_temp)
+    nuclei_temp=np.concatenate(nuclei_temp,axis=0)
+
+    bound_temp=np.array(bound_temp)
+    bound_temp=np.concatenate(bound_temp,axis=0)
+
+    nuclei_temp=nuclei_temp[pad_r1:nuclei_temp.shape[0]-pad_r2,pad_c1:nuclei_temp.shape[1]-pad_c2]*255
+    bound_temp=bound_temp[pad_r1:bound_temp.shape[0]-pad_r2,pad_c1:bound_temp.shape[1]-pad_c2]*255
+
+    nuclei_temp=nuclei_temp.astype(np.uint8)
+    bound_temp=bound_temp.astype(np.uint8)
+
+    y_pred=np.stack((nuclei_temp,bound_temp),axis=0)
+    y_pred=np.expand_dims(y_pred,axis=0)
+#         nuclei_thresh=threshold_otsu(nuclei_temp)
+#         nucei_thresholded=nuclei_temp>nuclei_thresh
+
+    dice_score=whole_dice_metric(nuclei_temp,nuclei_mask)
+    
+    return nuclei_temp,bound_temp
 
 
 def whole_img_pred(IMAGE_PATH,img_name,model,print_prompt=False):
@@ -173,6 +258,53 @@ def post_process(PRED_PATH,patient_name,img_name,threshold='otsu',apply_qc=False
                                        
     return comb_img,metrics
 
+def post_process2(nuc_img,bound_img,patient_name,img_name,threshold='otsu',apply_qc=False,print_prompt=False):
+    
+    path_to_patient_data='Dapi_patient_data/{}'.format(patient_name)
+    
+#     selected_ip=imread(path_to_patient_data+'/ROI/'+img_name)
+    selected_binary_gt=imread(path_to_patient_data+'/nuc_mask/'+img_name)
+
+    gt_narray_name=\
+    '/datalab/training-assets/R_medical/atheeth/DAPI/nucleiseg_dapi/Dapi_patient_data/{}/NucSeg/NucMap_{}_{}.tif'.\
+    format(patient_name,patient_name,img_name.split('.')[0])
+    gt_narray_image=imread(gt_narray_name)
+    gt_narray_image=gt_narray_image.astype(np.uint16)
+    
+    
+#     bound_img_path=os.path.join(PRED_PATH,'{}_bound_'.format(patient_name)+img_name.split('.')[0]+'.png')
+#     bound_img=imread(bound_img_path)
+#     nuc_img_path=os.path.join(PRED_PATH,'{}_nuclei_'.format(patient_name)+img_name.split('.')[0]+'.png')
+#     nuc_img=imread(nuc_img_path)
+    comb_img=img_recon_consep(nuc_img,bound_img)
+    comb_img=comb_img.astype(np.uint16)
+
+#     plt.imshow(comb_img);plt.show()
+    if apply_qc:
+        qc_path='Dapi_patient_data/{}/OverallQCMasks'.format(patient_name)
+        qc_file_path=os.path.join(qc_path,'OverallQCMask_{}_{}.tif'.format(patient_name,img_name.split('.')[0]))
+        qc_mask=imread(qc_file_path)
+        qc_mask[qc_mask!=0]=1
+        comb_img=comb_img*qc_mask
+        del qc_mask
+    
+
+
+    gt_image=remap_label(gt_narray_image)
+    total=len(np.unique(gt_image))
+    del gt_narray_image,nuc_img,bound_img
+    comb_img=remap_label(comb_img)
+    list_1=None
+
+    list_q,_,list_cm=get_fast_pq(gt_image, comb_img)
+    gt_image=(gt_image>0).astype(np.uint8)
+    comb_img_dice=(comb_img>0).astype(np.uint8)
+    dice=whole_dice_metric(comb_img_dice,gt_image)
+    del comb_img_dice
+    metrics=[list_q,list_cm,dice,total]
+                                       
+    return comb_img,metrics
+
 
 def coord2array(coord):
     x=[]
@@ -181,6 +313,7 @@ def coord2array(coord):
         x.append(i[0])
         y.append(i[1])
     return (x,y)
+
 
 
 def img_recon(nuclei,boundary,print_prompt=True):
---
+++ test-Copy1.py
@@ -0,0 +1,57 @@
+from Utils.Prediction import post_process,whole_img_pred
+from tqdm import tqdm
+import torch
+from Models.ModelsTorch import AttnUNet,load_model
+import torch.nn.functional as F
+import os 
+import pandas  as pd
+
+import shutil
+import random
+
+
+
+
+patient_list=[x for x in os.listdir('Dapi_patient_data/') if 'S-' in x and '.zip' not in x]
+print("Available slides :-\n ",*patient_list,sep='\n')
+patient_name=input("ENTER PATIENT SLIDE ID : ")
+assert patient_name in patient_list,"Please enter valid ID  out of the list {}".format(patient_list)
+IMAGE_PATH='/datalab/training-assets/R_medical/atheeth/DAPI/nucleiseg_dapi/Dapi_patient_data/{}'.format(patient_name)
+img_list=os.listdir(IMAGE_PATH+'/ROI')
+
+
+    
+print("Selected RoIs :- \n",*img_list,sep='\n')
+pred_dir_name=os.path.join(IMAGE_PATH,'predictions')
+processed_dir=os.path.join(IMAGE_PATH,'processed')
+if not os.path.exists(pred_dir_name):
+    os.mkdir(pred_dir_name)
+if not os.path.exists(processed_dir):
+    os.mkdir(processed_dir)
+    
+    
+
+model=AttnUNet(img_ch=1,output_ch=2)
+load_model('model_2019_11_13/model_optim.pth',model)
+
+patient=patient_name
+patient_list=[x for x in os.listdir('Dapi_patient_data/') if patient_name in x and '.zip' not in x]
+for patient in patient_list:
+    whole_img_pred(IMAGE_PATH,img_list,pred_dir_name,model,print_prompt=True)
+    
+    
+    
+pred_dir=os.path.join(os.getcwd(),pred_dir_name)
+
+if not os.path.exists(pred_dir):
+    os.mkdir(pred_dir)
+    print("Made {} directory".format(pred_dir.split('/')[-1]))
+else:
+    print("{} directory already exists in {}".format(pred_dir.split('/')[-1],'/'.join(pred_dir.split('/')[:-1])))
+
+loop=tqdm(img_list)
+for count,img_name in enumerate(loop):
+    dice,nuclei_temp,bound_temp=whole_img_pred(IMAGE_PATH,img_name,model,print_prompt=False)
+    
+    imsave(pred_dir_name+'/bound_'+img_name.split('.')[0]+'.png',bound_temp)
+    imsave(pred_dir_name+'/nuclei_'+img_name.split('.')[0]+'.png',nuclei_temp)
